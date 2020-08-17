3e0c007bd3c265ecb8b9da3ff4bbd167fc5aa584
Ryan Bressette <bressetteryan@gmail.com>
2020-08-16 20:38:56 -0400

    add menu for body content

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const crypto = require('crypto');
 const fs = require('fs');
+const api = require('./src/domain/rest-api/restApi.js');
+
+const userAPI = new api('./src/domain/users.json');
 
 const port = 3000;
 
@@ -10,18 +13,50 @@ let app = express();
 app.use(cors());
 let jsonParser = bodyParser.json();
 
-const menuPath = './src/app/menu.html'
+const menuPath = './src/app/menu.html';
+const appPath = './app.html';
 
 function getHtml(path) {
   return fs.readFileSync(path, 'utf8').toString();
 }
 
-app.get('/*', function(req, res) {
-  let page = getHtml(menuPath);
+function getHtmlFragment(fileName) {
+  return getHtml('./src/app/' + fileName + '.html');
+}
+
+// Use regex to find custom html tags and replace them with their content.
+function getApp(routingPage) {
+  
+  let html = getHtml(appPath).replace('<routing-module></routing-module>', getHtmlFragment(routingPage));
+
+  let regex = /<app-([a-z]+)><\/app-[a-z]+>/g;
+
+  console.log(html);
+
+  let arr;
+  
+  while ((arr = regex.exec(html)) !== null) {
+    
+    let prefix = html.substring(0, arr['index']);
+    let fragment = getHtmlFragment(arr[1]);
+    let suffix = html.substring(arr['index'] + arr[0].length, html.length);
+    html = prefix + fragment + suffix;
+  }
+
+  return html;
+}
+
+app.get('/login', function(req, res) {
+  let page = getApp('login');
   res.status(200).send(page);
 });
 
+app.get('/*', function(req, res) {
+  let page = getApp('home');
+  res.status(200).send(page);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
